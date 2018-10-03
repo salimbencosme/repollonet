0c83d06b5f2e4880af305def5484b994780e9ca8
Salim <sbencosme@encorerepair.com>
2018-10-03 14:43:54 -0400

    first message

---
+++ src/containers/Main.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+import Nav from '../components/Nav';
+
+
+class Main extends Component{
+
+    render(){
+
+        return(
+
+            <div>
+                <Nav classparam="nav-bar-background" />
+
+                <h1>DEDE AQUI BROTHER</h1>
+            </div>    
+
+        );
+
+    }
+
+
+}
+export default Main;
