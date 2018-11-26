8a52bff3d2f5af7f124e25d1bdaf921c25d2377a
Salim <sbencosme@encorerepair.com>
2018-11-26 14:32:52 -0400

    Updates

---
+++ precache-manifest.47021e4ac7696a3cc2e5a86c673b8219.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "2942bd3fb81b3b133bd1cb9fe8f31b1f",
+    "url": "/repollonet/static/media/recipes-drinks.2942bd3f.png"
+  },
+  {
+    "revision": "00bc0bc92c244afe067f",
+    "url": "/repollonet/static/css/main.5adb7dc0.chunk.css"
+  },
+  {
+    "revision": "81288256423164f8ecef06062ab9b81d",
+    "url": "/repollonet/static/media/cancel.81288256.png"
+  },
+  {
+    "revision": "6b0afd376032eb2a4949",
+    "url": "/repollonet/static/js/1.6b0afd37.chunk.js"
+  },
+  {
+    "revision": "9f7cb4b96abb45d5b9b4",
+    "url": "/repollonet/static/js/runtime~main.9f7cb4b9.js"
+  },
+  {
+    "revision": "24850be791859439f75818fd011a5fa3",
+    "url": "/repollonet/static/media/tips.24850be7.png"
+  },
+  {
+    "revision": "b316369a5ee165838ba4eb7ac6f062c1",
+    "url": "/repollonet/static/media/questions.b316369a.png"
+  },
+  {
+    "revision": "c00169032b7e6203d49b469c30295588",
+    "url": "/repollonet/static/media/recipes.c0016903.png"
+  },
+  {
+    "revision": "a10b6e290e5bb5e088eb1f2cc2ff8495",
+    "url": "/repollonet/static/media/feature.a10b6e29.png"
+  },
+  {
+    "revision": "00bc0bc92c244afe067f",
+    "url": "/repollonet/static/js/main.00bc0bc9.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/repollonet/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/repollonet/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/repollonet/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/repollonet/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/repollonet/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "399d66fc571618130cf0a72c9a68ac5c",
+    "url": "/repollonet/static/media/donation.399d66fc.png"
+  },
+  {
+    "revision": "f66282f51145bf866410a0374cde6248",
+    "url": "/repollonet/static/media/thankyou.f66282f5.png"
+  },
+  {
+    "revision": "6b0afd376032eb2a4949",
+    "url": "/repollonet/static/css/1.59ec481c.chunk.css"
+  },
+  {
+    "revision": "6c2b5906a55712f8cf1891b285ed3e36",
+    "url": "/repollonet/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/repollonet/precache-manifest.e975fc3093d207db407601403870012d.js"
+  "/repollonet/precache-manifest.47021e4ac7696a3cc2e5a86c673b8219.js"
 );
 
 workbox.clientsClaim();
