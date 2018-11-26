fa4c719c5361375274ac6be749da90a7557c70e3
Salim <sbencosme@encorerepair.com>
2018-11-26 13:53:49 -0400

    More pages done

--- src/components/DonateWrong.js
+++ src/components/DonateWrong.js
@@ -1,4 +1,15 @@
 import React,{Component} from 'react';
+import img from '../resources/img/cancel.png';
+import { Link } from "react-router-dom";
+
+var linkStyle = {
+    'text-decoration':'underline'
+  };
+
+var label={
+    'font-size': '2em',
+    'color': '#f30108'
+}
 
 class DonateWrong extends Component{
 
@@ -7,7 +18,12 @@ class DonateWrong extends Component{
     }
     render(){
         return(
-            <h1>DONATE WRONG</h1>
+            <div>
+                <center>
+                <img className="img-small" src={img} />
+                <label style={label}>We regret that you have canceled your donation, if something happened or you have something to communicate, please do not hesitate to contact us.  <Link style={linkStyle} to="contact">Click here to tell us</Link></label>
+                </center>
+            </div>
         );
     }
 }
