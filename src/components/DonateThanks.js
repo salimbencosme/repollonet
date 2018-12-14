be220661e3f5b66264425393de6b5c83c89bbd2c
Salim <sbencosme@encorerepair.com>
2018-12-14 15:18:26 -0400

    modifed pages left

--- src/components/DonateThanks.js
+++ src/components/DonateThanks.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import img from '../resources/img/thankyou.png';
+import imgES from '../resources/img/thankyou-es.png';
 import { Link } from "react-router-dom";
+import themeHandler from '../common/ThemeHandler';
+import {getSelectedLanguage } from '../common/Utils';
 
 class DonateThanks extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            language: 'english'
+        }
     }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({ language: getSelectedLanguage() });
+    }
+
+    componentDidMount() {
+        themeHandler("default");
+        this.setState({ language: getSelectedLanguage() });
+    }
+
     render() {
         return (
             <Link to="/post/all">
                 <center>
-                    <img className="img-welcome" src={img} />
+                    <img style={{ width: '600px'}} className="img-welcome" src={this.state.language === 'english' ?  img : imgES} />
                 </center>
             </Link>
         );
--- src/components/DonateWrong.js
+++ src/components/DonateWrong.js
@@ -1,6 +1,8 @@
 import React,{Component} from 'react';
 import img from '../resources/img/cancel.png';
 import { Link } from "react-router-dom";
+import themeHandler from '../common/ThemeHandler';
+import { manageLanguage, getSelectedLanguage } from '../common/Utils';
 
 var linkStyle = {
     'text-decoration':'underline'
@@ -15,13 +17,27 @@ class DonateWrong extends Component{
 
     constructor(props){
         super(props);
+        this.state = {
+            language: 'english'
+        }
     }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({ language: getSelectedLanguage() });
+    }
+
+    componentDidMount() {
+        themeHandler("default");
+        this.setState({ language: getSelectedLanguage() });
+    }
+
+
     render(){
         return(
             <div>
                 <center>
                 <img className="img-small" src={img} />
-                <label style={label}>We regret that you have canceled your donation, if something happened or you have something to communicate, please do not hesitate to contact us.  <Link style={linkStyle} to="contact">Click here to tell us</Link></label>
+                <label style={label}>{manageLanguage(this.state.language,'Lamentamos que haya cancelado su donación, si sucedió algo o si tiene algo que comunicar, no dude en ponerse en contacto con nosotros.','We regret that you have canceled your donation, if something happened or you have something to communicate, please do not hesitate to contact us.')}  <Link style={linkStyle} to="contact">{manageLanguage(this.state.language,'Haga clic aquí para decirnos','Click here to tell us')}</Link></label>
                 </center>
             </div>
         );
