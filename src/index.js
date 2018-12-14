e87f9957b10f48665221ed62216838321dfc3963
Salim <sbencosme@encorerepair.com>
2018-12-14 11:35:53 -0400

    TEST

--- src/index.js
+++ src/index.js
@@ -16,20 +16,22 @@ import DonateThanks from './components/DonateThanks';
 import DonateWrong from './components/DonateWrong';
 import './resources/css/main.css';
 ReactDOM.render(
-    <Router>
+
+
+    <Router  basename={process.env.PUBLIC_URL}>
         <Main>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/post/:typecontent" component={Post} />
-            <Route path="/details/:typecontent/:id" component={PostDetails} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/subscribe" component={Subscribe} />
-            <Route path={`${process.env.PUBLIC_URL}/donate`} component={Donate} />
-            <Route path="/donatethanks" component={DonateThanks} />
-            <Route path="/donatewrong" component={DonateWrong} />
-            <Route name="wrong-url" component={AppDefault} />
-        </Switch>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/post/:typecontent" component={Post} />
+                <Route path="/details/:typecontent/:id" component={PostDetails} />
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/subscribe" component={Subscribe} />
+                <Route path="/donate" component={Donate} />
+                <Route path="/donatethanks" component={DonateThanks} />
+                <Route path="/donatewrong" component={DonateWrong} />
+                <Route name="wrong-url" component={AppDefault} />
+            </Switch>
         </Main>
     </Router>
 
