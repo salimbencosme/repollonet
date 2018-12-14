15fabe4d104a791c2f8bfe6307abb7236b1af1f6
Salim <sbencosme@encorerepair.com>
2018-12-14 14:48:18 -0400

    wrong url added it

--- src/AppDefault.js
+++ src/AppDefault.js
@@ -2,21 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Link } from "react-router-dom";
-
+import worngImgEN from '../src/resources/img/wrongurl-en.png';
+import worngImgES from '../src/resources/img/wrongurl-es.png';
+import { getSelectedLanguage } from './common/Utils';
+import themeHandler from './common/ThemeHandler';
 
 class AppDefault extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
+    this.state = {
+      language: 'english'
+    }
+  }
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({ language: getSelectedLanguage() });
+  }
+
+  componentDidMount() {
+    themeHandler("default");
+    this.setState({ language: getSelectedLanguage() });
   }
 
   render() {
     return (
-      <div>
+      <div class="div-container-scroll">
+
         <Link to="/">
-        <img className="img-welcome" src="http://www.zinavo-demo.in/wrong-url.jpg" />
+          <img className="img-welcome" src={this.state.language === 'english' ? worngImgEN : worngImgES} />
         </Link>
+        <br />
+
+        <br />
+        <br />
+
+        <br />
+        <br />
+
+        <br />
+        <br />
       </div>
     );
   }
